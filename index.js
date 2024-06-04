4dae4d6811a475944c686bc575d2039cb488e26d
naveennv2k <78138836+naveennv2k@users.noreply.github.com>
2024-06-04 11:21:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import * as core from "@actions/core";
+const core = require('@actions/core');
+
 const envName = 'MY_VARIABLE';
 const secretValue = 'its working';
 core.exportVariable(envName, secretValue);
