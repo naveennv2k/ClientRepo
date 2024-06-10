dad72b6044a0d20983cd069e795e84df76454841
naveennv2k <78138836+naveennv2k@users.noreply.github.com>
2024-06-10 17:18:12 +0530

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-curl  https://downloads.zohocdn.com/vault-cli-desktop/macos/zv_cli.zip --output zv_cli.zip
+curl  https://downloads.zohocdn.com/vault-cli-desktop/linux/zv_cli.zip --output zv_cli.zip
 unzip zv_cli.zip
 rm zv_cli.zip
+#!/bin/bash
+set -e
+
+# Install op-cli
+install_op_cli() {
+  # Create a temporary directory where the CLI is installed
+    
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    curl  https://downloads.zohocdn.com/vault-cli-desktop/linux/zv_cli.zip --output zv_cli.zip
+    unzip zv_cli.zip
+    rm zv_cli.zip
+    
+
+    if [[ "$ARCH" != "386" ]] && [[ "$ARCH" != "amd64" ]] && [[ "$ARCH" != "arm" ]] && [[ "$ARCH" != "arm64" ]]; then
+      echo "Unsupported architecture for the 1Password CLI: $ARCH."
+      exit 1
+    fi
+
+    
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    curl  https://downloads.zohocdn.com/vault-cli-desktop/macos/zv_cli.zip --output zv_cli.zip
+    unzip zv_cli.zip
+    rm zv_cli.zip
+    
+  else
+    echo "Operating system not supported yet for this GitHub Action: $OSTYPE."
+    exit 1
+  fi
+}
+
+install_op_cli
